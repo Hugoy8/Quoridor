38e174df1cd525c93d5319e5518e2439b0b506e4
Adri1Cz <118809539+Adri1Cz@users.noreply.github.com>
2023-05-31 19:44:49 +0200

    fix

--- domain/bot/bot.py
+++ domain/bot/bot.py
@@ -15,21 +15,21 @@ class Bot :
     def botPlaysRandom(self):
         action = self.randomChoice(["move","build"])
         if action == "build"  and self.board.playerHasFence() == True and self.board.allPossibleBuildFence() !=[]:
-            self.botBuildRandomFence(self.board.allPossibleBuildFence())
-        else :
-            movement = self.randomChoice(self.board.allPossibleMoveForPlayer())
-            self.board.move(movement[0],movement[1])
-        
-    
+            build = self.botBuildRandomFence(self.board.allPossibleBuildFence())
+            if build != False :
+                return ["build", build]
+        return ["move",self.randomChoice(self.board.allPossibleMoveForPlayer())]
+            
+            
     def currentBotPlaysBasedOnDifficulty(self, difficulty):
         if difficulty ==  1 :
-            self.botPlaysRandom()
+            action = self.botPlaysRandom()
+        self.doAction(action)
             
-    
+            
     def botBuildRandomFence(self, allPossibleBuildFence):
-        can_build = False
         possibleBuildFence = allPossibleBuildFence
-        while can_build == False and possibleBuildFence !=[]:
+        while possibleBuildFence !=[]:
             build = self.randomChoice(possibleBuildFence)
             x_co_fence = build[0]
             y_co_fence = build[1]
@@ -42,7 +42,20 @@ class Bot :
             if self.board.fenceNotCloseAccesGoal()==False :
                 possibleBuildFence.remove(build)
                 self.board.deBuildFence(x_co_fence,y_co_fence)
-                self.board.displayBoard(False) 
             else : 
-                can_build = True
+                self.board.deBuildFence(x_co_fence,y_co_fence)
+                return [x_co_fence, y_co_fence, orientation]
+        return False
+    
+    
+    def doAction(self, action):
+        if action[0] == "move":
+            self.board.move(action[1][0],action[1][1])
+        else :
+            if action[1][2] == "vertical":
+                self.fence_orientation = "vertical"
+            else :
+                self.fence_orientation = "horizontal"
+            self.board.buildFence(action[1][0],action[1][1])
+            
     
